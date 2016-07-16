8c9f00bccb3bf57c666725e6b688da144e3e632d
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 21:54:22 +1200

    Updated lambci build script

--- lambci.sh
+++ lambci.sh
@@ -6,10 +6,10 @@ set -o errexit
 docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker images
 docker save "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM-layered" > layered.tar
-sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM"
+docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM"
 cat squashed.tar | docker load
 docker images
 docker push "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM"
-sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:latest"
+docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:latest"
 cat squashed.tar | docker load
 docker push "66pix/nginx-pagespeed:latest"
