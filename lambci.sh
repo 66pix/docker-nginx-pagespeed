25b552371ed6bdc1dbbc6b055e1ed6467c10acaa
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 23:27:01 +1200

    Updated lambci build script

--- lambci.sh
+++ lambci.sh
@@ -5,7 +5,8 @@ set -o errexit
 
 LAMBCI_BUILD_NUM_TRIMMED="$(echo -e "${LAMBCI_BUILD_NUM}" | tr -d '[[:space:]]')"
 NAME="66pix/nginx-pagespeed"
-TAG="${NAME}:${LAMBCI_BUILD_NUM_TRIMMED}"
+TAG="${NAME}:${LAMBCI_BUILD_NUM_TRIMMED}-layered"
+TAG_SQUASHED="${NAME}:${LAMBCI_BUILD_NUM_TRIMMED}"
 TAG_LATEST="${NAME}:latest"
 
 echo "Tag: $TAG"
@@ -16,11 +17,14 @@ docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
 echo "Building $TAG"
 docker build -t "${TAG}" .
 
-echo "Pushing $TAG"
-docker push "$TAG"
+docker export $(docker images -q $TAG) | docker import - $TAG_SQUASHED
+docker export $(docker images -q $TAG) | docker import - $TAG_LATEST
 
-echo "Building $TAG_LATEST"
-docker build -t "$TAG_LATEST" .
+echo "Image check"
+docker images $TAG_SQUASHED
+
+echo "Pushing $TAG_SQUASHED"
+docker push "$TAG_SQUASHED"
 
 echo "Pushing latest"
 docker push "$TAG_LATEST"
