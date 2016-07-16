341974f6bd4a7270b48d703cf2186b269d0a0300
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 21:49:47 +1200

    Updated lambci build script

--- lambci.sh
+++ lambci.sh
@@ -7,13 +7,13 @@ printenv
 
 exit 1
 
-# docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
-# docker images
-# docker save "66pix/nginx-pagespeed:$CIRCLE_BUILD_NUM-layered" > layered.tar
-# sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:$CIRCLE_BUILD_NUM"
-# cat squashed.tar | docker load
-# docker images
-# docker push "66pix/nginx-pagespeed:$CIRCLE_BUILD_NUM"
-# sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:latest"
-# cat squashed.tar | docker load
-# docker push "66pix/nginx-pagespeed:latest"
+docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker images
+docker save "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM-layered" > layered.tar
+sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM"
+cat squashed.tar | docker load
+docker images
+docker push "66pix/nginx-pagespeed:$LAMBCI_BUILD_NUM"
+sudo docker-squash -from root -i layered.tar -o squashed.tar -t "66pix/nginx-pagespeed:latest"
+cat squashed.tar | docker load
+docker push "66pix/nginx-pagespeed:latest"
